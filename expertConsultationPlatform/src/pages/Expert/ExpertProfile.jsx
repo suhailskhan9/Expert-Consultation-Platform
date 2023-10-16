d398f61b8951d4068b3bc6f72afd2516449d7200
SP <prajwalw02@gmail.com>
2023-10-16 22:34:11 +0530

    changes to profile

--- expertConsultationPlatform/src/pages/Expert/ExpertProfile.jsx
+++ expertConsultationPlatform/src/pages/Expert/ExpertProfile.jsx
@@ -1,129 +1,130 @@
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import Sidebar, { SidebarItem } from "../../components/Sidebar";
-import {LifeBuoy,User,Calendar,Inbox, Receipt, Boxes, Package, UserCircle, BarChart3, LayoutDashboard, Settings, Mail, IndianRupee, LogOut, Video, MessageCircle} from "lucide-react";
+import { User, Calendar, Inbox, MessageCircle, Video, LogOut, Settings, LifeBuoy } from "lucide-react";
 import axios from "axios";
 
+export default function ExpertProfile() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    specialization: "",
+    // Add more fields as needed
+  });
 
-export default function ExpertProfile(){
-    const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        specialization: "",
-        // Add more fields as needed
+  const location = useLocation();
+
+  useEffect(() => {
+    const loggedInExpertEmail = location.state?.email;
+
+    axios
+  .get(`http://localhost:5000/getExpertData?email=${loggedInExpertEmail}`)
+  .then((response) => {
+    // Check if the response is an array and has at least one element
+    if (Array.isArray(response.data) && response.data.length > 0) {
+      const ExpertData = response.data[0]; // Access the first element
+      console.log(ExpertData);
+      console.log(ExpertData.username);
+      console.log(ExpertData.email);
+      setFormData({
+        name: ExpertData.username,
+        email: ExpertData.email,
+        specialization: ExpertData.specialization
       });
+    } else {
+      console.log("No user data found in the response.");
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}, [location]);
 
-      const handleChange = (e) => {
-        setFormData({
-          ...formData,
-          [e.target.name]: e.target.value,
-        });
-      };
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        // Handle form submission, e.g., send data to a server
-        console.log("Form submitted with data:", formData);
-      };
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-      const [userData, setUserData] = useState([])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission, e.g., send data to a server
+    console.log("Form submitted with data:", formData);
+  };
 
-      useEffect(() => {
-        axios.get('http://localhost:5000/getUserData')
-        .then(userData => setUserData(userData.data))
-        .then(console.log(userData.username))  
-        .catch(err => console.log(err))
-      }, [])
-
-
-
-
-return(
-
-
+  return (
     <div className="flex">
+      <Sidebar className="w-64">
+        <SidebarItem
+          icon={<User size={20} />}
+          text="Profile"
+          active
+        />
+        <SidebarItem icon={<Calendar size={20} />} text="Calendar" />
+        <SidebarItem icon={<Inbox size={20} />} text="Inbox" />
+        <SidebarItem icon={<MessageCircle size={20} />} text="Chat" />
+        <SidebarItem icon={<Video size={20} />} text="Video Call" alert />
+        <SidebarItem icon={<LogOut size={20} />} text="Log Out" />
+        <hr className="my-3" />
+        <SidebarItem icon={<Settings size={20} />} text="Settings" />
+        <SidebarItem icon={<LifeBuoy size={20} />} text="Help" />
+      </Sidebar>
 
-
-        <Sidebar className="w-64">
-            <SidebarItem
-                icon = {<User size = {20} />}
-                text = "Profile"
-                active
-                />
-            <SidebarItem icon = {<Calendar size={20}/>} text="Calendar" />
-            <SidebarItem icon = {<Inbox size={20} />} text="Inbox" />
-            <SidebarItem icon = {<MessageCircle size={20} />} text="Chat"  />
-            <SidebarItem icon = {<Video size={20} />} text="Video Call" alert />
-            <SidebarItem icon = {<LogOut size={20} />} text="Log Out" />
-            <hr className="my-3"/>
-            <SidebarItem icon={<Settings size={20} />} text="Settings" />
-            <SidebarItem icon={<LifeBuoy size={20} />} text="Help" />
-        </Sidebar>
-
-        <div className="flex-1 p-4">
+      <div className="flex-1 p-4">
         <p>This is the content of the right side container.</p>
         {/* Add more content as needed */}
-
-
-       {/* Right side container with user profile form */}
-      <div className="flex-1 p-4">
-        <h2 className="text-2xl font-semibold mb-4">Edit Profile</h2>
-        <form onSubmit={handleSubmit} className="max-w-md space-y-4">
-          <div>
-            <label htmlFor="name" className="block text-sm font-medium text-gray-600">
-              Name:
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={userData.username}
-              onChange={handleChange}
-              className="mt-1 p-2 w-full border rounded-md"
-              required
-            />
-          </div>
-
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-600">
-              Email:
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              defaultValue={userData.email}
-              onChange={handleChange}
-              className="mt-1 p-2 w-full border rounded-md"
-              required
-              
-            />
-          </div>
-
-          <div>
-            <label htmlFor="specialization" className="block text-sm font-medium text-gray-600">
-              Specialization:
-            </label>
-            <input
-              type="text"
-              id="specialization"
-              name="specialization"
-              value={formData.specialization}
-              onChange={handleChange}
-              className="mt-1 p-2 w-full border rounded-md"
-              required
-            />
-          </div>
-
-          {/* Add more form fields as needed */}
-          
-          <button type="submit" className="bg-indigo-500 text-white p-2 rounded-md hover:bg-indigo-600">
-            Submit
-          </button>
-        </form>
-      </div>
-
-
+        <div className="flex-1 p-4">
+          <h2 className="text-2xl font-semibold mb-4">Edit Profile</h2>
+          <form onSubmit={handleSubmit} className="max-w-md space-y-4">
+            <div>
+              <label htmlFor="name" className="block text-sm font-medium text-gray-600">
+                Name:
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className="mt-1 p-2 w-full border rounded-md"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium text-gray-600">
+                Email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="mt-1 p-2 w-full border rounded-md"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="specialization" className="block text-sm font-medium text-gray-600">
+                Specialization:
+              </label>
+              <input
+                type="text"
+                id="specialization"
+                name="specialization"
+                value={formData.specialization}
+                onChange={handleChange}
+                className="mt-1 p-2 w-full border rounded-md"
+                required
+              />
+            </div>
+            <button type="submit" className="bg-indigo-500 text-white p-2 rounded-md hover-bg-indigo-600">
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-           )
-    }
+  );
+}
--- expertConsultationPlatform/src/pages/User/UserProfile.jsx
+++ expertConsultationPlatform/src/pages/User/UserProfile.jsx
@@ -1,118 +1,114 @@
 import Sidebar, { SidebarItem } from "../../components/Sidebar";
-import {LifeBuoy,User,Calendar,Inbox, Receipt, Boxes, Package, UserCircle, BarChart3, LayoutDashboard, Settings, Mail, IndianRupee, LogOut, Video, Search, MessageCircle} from "lucide-react";
-import { useState } from "react";
+import { User, Search, Inbox, MessageCircle, Video, LogOut, Settings, LifeBuoy } from "lucide-react";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
-export default function UserProfile(){
+export default function UserProfile() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
 
-    const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        specialization: "",
-        // Add more fields as needed
+  const location = useLocation();
+
+  useEffect(() => {
+    const loggedInUserEmail = location.state?.email;
+
+    axios
+  .get(`http://localhost:5000/getUserData?email=${loggedInUserEmail}`)
+  .then((response) => {
+    // Check if the response is an array and has at least one element
+    if (Array.isArray(response.data) && response.data.length > 0) {
+      const userData = response.data[0]; // Access the first element
+      console.log(userData);
+      console.log(userData.username);
+      console.log(userData.email);
+      setFormData({
+        name: userData.username,
+        email: userData.email,
       });
-
-      const handleChange = (e) => {
-        setFormData({
-          ...formData,
-          [e.target.name]: e.target.value,
-        });
-      };
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        // Handle form submission, e.g., send data to a server
-        console.log("Form submitted with data:", formData);
-      };
+    } else {
+      console.log("No user data found in the response.");
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}, [location]);
 
 
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-return(
+  const handleEditClick = () => {
+    setEditMode(true);
+  };
 
+  const handleSaveClick = () => {
+    setEditMode(false);
+    // Handle form submission, e.g., send data to a server
+    console.log("Form submitted with data:", formData);
+  };
+
+  return (
     <div className="flex">
+      <Sidebar className="w-64">
+        <SidebarItem icon={<User size={20} />} text="Profile" active />
+        <SidebarItem icon={<Search size={20} />} text="Browse Experts" />
+        <SidebarItem icon={<Inbox size={20} />} text="Inbox" />
+        <SidebarItem icon={<MessageCircle size={20} />} text="Chat" />
+        <SidebarItem icon={<Video size={20} />} text="Video Call" alert />
+        <SidebarItem icon={<LogOut />} text="Log Out" />
+        <hr className="my-3" />
+        <SidebarItem icon={<Settings size={20} />} text="Settings" />
+        <SidebarItem icon={<LifeBuoy size={20} />} text="Help" />
+      </Sidebar>
 
-
-        <Sidebar className="w-64">
-            <SidebarItem
-                icon = {<User size = {20} />}
-                text = "Profile"
-                active
-                />
-            <SidebarItem icon = {<Search size={20}/>} text="Browse Experts"  />
-            <SidebarItem icon = {<Inbox size={20} />} text="Inbox" />
-            <SidebarItem icon = {<MessageCircle size={20} />} text="Chat"  />
-            <SidebarItem icon = {<Video size={20} />} text="Video Call" alert />
-            <SidebarItem icon = {<LogOut  />} text="Log Out" />
-            <hr className="my-3"/>
-            <SidebarItem icon={<Settings size={20} />} text="Settings" />
-            <SidebarItem icon={<LifeBuoy size={20} />} text="Help" />
-        </Sidebar>
-
-        <div className="flex-1 p-4">
-        <p>This is the content of the right side container.</p>
-        {/* Add more content as needed */}
-
-        {/* Right side container with user profile form */}
       <div className="flex-1 p-4">
         <h2 className="text-2xl font-semibold mb-4">User Profile</h2>
-        <form onSubmit={handleSubmit} className="max-w-md space-y-4">
-          <div>
-            <label htmlFor="name" className="block text-sm font-medium text-gray-600">
-              Name:
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              className="mt-1 p-2 w-full border rounded-md"
-              required
-            />
-          </div>
 
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-600">
-              Email:
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className="mt-1 p-2 w-full border rounded-md"
-              required
-            />
-          </div>
+            <form onSubmit={handleSaveClick} className="max-w-md space-y-4">
+            <div>
+              <label htmlFor="name" className="block text-sm font-medium text-gray-600">
+                Name:
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className="mt-1 p-2 w-full border rounded-md"
+                required
+              />
+            </div>
 
-          {/* <div>
-            <label htmlFor="specialization" className="block text-sm font-medium text-gray-600">
-              Specialization:
-            </label>
-            <input
-              type="text"
-              id="specialization"
-              name="specialization"
-              value={formData.specialization}
-              onChange={handleChange}
-              className="mt-1 p-2 w-full border rounded-md"
-              required
-            />
-          </div> */}
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium text-gray-600">
+                Email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="mt-1 p-2 w-full border rounded-md"
+                required
+              />
+            </div>
 
-          {/* Add more form fields as needed */}
-          
-          <button type="submit" className="bg-indigo-500 text-white p-2 rounded-md hover:bg-indigo-600">
-            Submit
-          </button>
-        </form>
-      </div>
-
-
-
-
-
+            {/* Add more form fields as needed */}
+            <button type="submit" className="bg-indigo-500 text-white p-2 rounded-md hover:bg-indigo-600">
+              Save
+            </button>
+          </form> 
       </div>
     </div>
-           )
-    }
+  );
+}
