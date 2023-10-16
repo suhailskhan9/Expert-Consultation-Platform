081d64387f25ddc51de9b625ebb5dd35d49f2b37
Iliyas-shah <shahiliyas289@gmail.com>
2023-10-16 20:41:39 +0530

    changes

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -1,4 +1,29 @@
 
+// import React, { useContext } from 'react';
+// import { Button } from '@material-ui/core';
+
+// import { SocketContext } from '../SocketContext';
+
+// const Notifications = () => {
+//   const { answerCall, call, callAccepted } = useContext(SocketContext);
+
+//   return (
+//     <>
+//       {!callAccepted && call.isReceivedCall  &&(
+//         <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+//           <h1>{call.name} is calling:</h1>
+//           <Button variant="contained" color="primary" onClick={answerCall}>
+//             Answer
+//           </Button>
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+// export default Notifications;
+
+
 import React, { useContext } from 'react';
 import { Button } from '@material-ui/core';
 
@@ -9,7 +34,7 @@ const Notifications = () => {
 
   return (
     <>
-      {!callAccepted && call.isReceivedCall  &&(
+      {call.isReceivingCall && !callAccepted && (
         <div style={{ display: 'flex', justifyContent: 'space-around' }}>
           <h1>{call.name} is calling:</h1>
           <Button variant="contained" color="primary" onClick={answerCall}>
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,18 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+// import { createRoot } from 'react-dom/client';
 import App from './App';
 import { ContextProvider } from './SocketContext';
-
 import './styles.css';
 
+// const root=document.getElementById('root');
+// const rootElement=createRoot(root);
+// rootElement.render(
+//   <ContextProvider>
+//     <App />
+//   </ContextProvider>
+// )
 ReactDOM.render(
   <ContextProvider>
     <App />
